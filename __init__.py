237b61c7121c53a5edec5082d296beac7ac908fc
mgracz <matt.gracz@gmail.com>
2019-07-25 08:53:45 -0500

    Added some standard python application start-up code.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 from sys import argv
 from main import RunManipuLogic as runApplication
 
-doRunUnitTests : bool = True
-try:
-    doRunUnitTests = doRunUnitTests or argv[bool(1)]
-except:
-    pass
+if __name__ == "__main__":
+    doRunUnitTests : bool = True
+    try:
+        doRunUnitTests = doRunUnitTests or argv[bool(1)]
+    except:
+        doRunUnitTests = False
 
-runApplication(doRunUnitTests)
+    runApplication(doRunUnitTests)
