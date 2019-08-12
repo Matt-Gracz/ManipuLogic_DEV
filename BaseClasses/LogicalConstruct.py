5650e11b9089bd9bd138ca55a435d5ffc8202b59
mgracz <matt.gracz@gmail.com>
2019-08-12 10:56:41 -0500

    Unit test for LogicalConstruct; no implementation necessary as it is a pure virtual/abstract class.

--- BaseClasses/LogicalConstruct.py
+++ BaseClasses/LogicalConstruct.py
@@ -1,15 +1,32 @@
 from UnitTesting import UnitTesting
+from io import StringIO
+import sys
 
 class LogicalConstruct(object):
-    """description of class"""
-    def toString() -> str:
+    """Root class for backend semantics classes."""
+
+    def toString(self) -> str:
+        """returns as string reprsentation of an instantiation"""
         pass
-    def print() -> None:
+    def print(self) -> None:
+        """prints to stdout the return value of self.toString()"""
         pass
     
-    def runTest(resultsAddFunction):
-        
-        resultsAddFunction("Test text")
-        return False
+    def runTest(resultsAddFunction) -> bool:
+        resultsAddFunction("print(): ")
+        didPass : bool = False
+        old_stdout : object = sys.stdout
+        #we'll use try/except to ensure stdout gets re-set no matter what before we exit this
+        #function
+        try:
+            expectedStr : str = self.toString()
+            sys.stdout = redirected_stdout = StringIO()
+            self.print()
+            testStr : str = redirected_stdout.getValue().strip()
+            didPass = didPass and (testStr == expectedStr)
+        except Exception as e:
+            pass
+        sys.stdout = old_stdout
+        return didPass
 
 
