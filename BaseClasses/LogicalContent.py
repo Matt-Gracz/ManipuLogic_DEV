e49479faca6d8ef73aece83e16c07e0e72559025
mgracz <matt.gracz@gmail.com>
2019-08-13 08:55:32 -0500

    Initial implementation of logical content

--- BaseClasses/LogicalContent.py
+++ BaseClasses/LogicalContent.py
@@ -1,7 +1,36 @@
 from BaseClasses.LogicalConstruct import LogicalConstruct
+from UnitTesting import UnitTesting, PassFailType
+
 class LogicalContent(LogicalConstruct):
     """description of class"""
     rawData : str = ""
     content = None
+
+    def __init__(self, *args, **kwargs):
+        argc = len(args)
+        if argc:
+            self.content = args[0]
+            try:
+                self.rawData = str(args[0])
+            except Exception as e:
+                pass
+        return super(self)
+
+    def __str__(self, *args, **kwargs) -> str:
+        return rawData
+        
+    def runTest(resultsAddFunction) -> None:
+        didPass : bool = False
+        error : bool = False
+        
+        pft : PassFailType
+        if(error):
+            pft = PassFailType.ERROR
+        elif(didPass):
+            pft = PassFailType.PASS
+        else:
+            pft = PassFailType.DEFECT
+
+        resultsAddFunction("print()", pft, "expected string: {} printed string: {}".format(expectedStr, testStr))
 
 
